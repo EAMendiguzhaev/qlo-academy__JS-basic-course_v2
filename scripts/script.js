109f1fda997d5d6792b2aa35a1c286f5b1803598
Eldar Arturovich M <77300300+BananaRepublic14@users.noreply.github.com>
2021-06-17 22:17:57 +0300

    Выполня ДЗ 17 урока (Пишем таймер)

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', () => {
+  let zeroTimer = null;
+
+  const getZeroTimer = (num) => {
+    if (num > 0 && num < 10) {
+      return '0' + num;
+    } else {
+      return num;
+    }
+  };
+
+  //Timer
+  const countTimer = (deadline) => {
+    const timerHours = document.querySelector('#timer-hours');
+    const timerMinutes = document.querySelector('#timer-minutes');
+    const timerSeconds = document.querySelector('#timer-seconds');
+
+    const getTimeRemaining = () => {
+      let dateStop = new Date(deadline).getTime();
+      let dateNow = new Date().getTime();
+      let timeRemaining = (dateStop - dateNow) / 1000;
+      let seconds = Math.floor(timeRemaining % 60);
+      let minutes = Math.floor((timeRemaining / 60) % 60);
+      let hours = Math.floor(timeRemaining / 60 / 60); // нужно добавить % 24, чтобы высчитать дни
+      // let day = Math.floor(timeRemaining / 60 / 60 / 24); // Высчитываeт дни
+
+      return { timeRemaining, hours, minutes, seconds };
+    };
+
+    const updateClock = () => {
+      const timer = getTimeRemaining();
+
+      if (timer.hours <= 0 && timer.minutes <= 0 && timer.seconds <= 0) {
+        clearInterval(zeroTimer);
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+      } else {
+        timerHours.textContent = getZeroTimer(timer.hours);
+        timerMinutes.textContent = getZeroTimer(timer.minutes);
+        timerSeconds.textContent = getZeroTimer(timer.seconds);
+      }
+    };
+
+    updateClock();
+    zeroTimer = setInterval(updateClock, 1000);
+  };
+
+  countTimer('18 june 2021');
+});
