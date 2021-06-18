105c3842770ee36ac17e06851feef10961c776b6
Eldar Arturovich M <77300300+BananaRepublic14@users.noreply.github.com>
2021-06-18 21:52:40 +0300

    Скрипты для меню и анимации для попап

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-  let zeroTimer = null;
-
-  const getZeroTimer = (num) => {
-    if (num > 0 && num < 10) {
-      return '0' + num;
-    } else {
-      return num;
-    }
-  };
+  const width = document.documentElement.clientWidth;
 
   //Timer
   const countTimer = (deadline) => {
@@ -25,6 +17,16 @@ window.addEventListener('DOMContentLoaded', () => {
       // let day = Math.floor(timeRemaining / 60 / 60 / 24); // Высчитываeт дни
 
       return { timeRemaining, hours, minutes, seconds };
+    };
+
+    let zeroTimer = null;
+
+    const getZeroTimer = (num) => {
+      if (num > 0 && num < 10) {
+        return '0' + num;
+      } else {
+        return num;
+      }
     };
 
     const updateClock = () => {
@@ -46,5 +48,88 @@ window.addEventListener('DOMContentLoaded', () => {
     zeroTimer = setInterval(updateClock, 1000);
   };
 
-  countTimer('18 june 2021');
+  // Меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu');
+    const menu = document.querySelector('menu');
+    const btnClose = document.querySelector('.close-btn');
+    const menuItems = menu.querySelectorAll('li');
+
+    const hendlerMenu = () => {
+      menu.classList.toggle('active-menu');
+
+      // Альтернативный способ
+      // if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+      //   menu.style.transform = `translate(0)`;
+      // } else {
+      //   menu.style.transform = `translate(-100%)`;
+      // }
+    };
+
+    btnMenu.addEventListener('click', () => {
+      hendlerMenu();
+    });
+
+    btnClose.addEventListener('click', () => {
+      hendlerMenu();
+    });
+
+    menuItems.forEach((elements) => elements.addEventListener('click', hendlerMenu));
+  };
+
+  // Попап
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup');
+    const btnPopUp = document.querySelectorAll('.popup-btn');
+    const btnPopUpClose = document.querySelector('.popup-close');
+    const popUpContent = document.querySelector('.popup-content');
+    let animationFrame;
+    let count = 0;
+
+    btnPopUp.forEach((elements) => {
+      elements.addEventListener('click', () => {
+        popup.style.display = 'block';
+
+        const popUpAnimation = () => {
+          animationFrame = requestAnimationFrame(popUpAnimation);
+          count++;
+
+          if (count < 38) {
+            popUpContent.style.left = count + '%';
+          } else {
+            cancelAnimationFrame(animationFrame);
+          }
+        };
+
+        if (width > 768) {
+          popUpAnimation();
+        }
+      });
+    });
+
+    btnPopUpClose.addEventListener('click', () => {
+      const popUpAnimation = () => {
+        animationFrame = requestAnimationFrame(popUpAnimation);
+        count--;
+        if (popUpContent.style.left > '0%') {
+          popUpContent.style.left = count + '%';
+        } else {
+          cancelAnimationFrame(animationFrame);
+        }
+
+        if (popUpContent.style.left === '0%') {
+          popup.style.display = 'none';
+        }
+      };
+      if (width > 768) {
+        popUpAnimation();
+      } else {
+        popup.style.display = 'none';
+      }
+    });
+  };
+
+  countTimer('20 june 2021');
+  toggleMenu();
+  togglePopUp();
 });
