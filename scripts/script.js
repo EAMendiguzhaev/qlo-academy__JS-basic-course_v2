b69edc61f3d18b561efd4457c78ea748f8b475b7
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-27 13:04:06 +0300

    Добавил ф-цию анимации и реализовал калькулятор

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,26 @@ window.addEventListener('DOMContentLoaded', () => {
   const width = document.documentElement.clientWidth;
   const btnScroll = document.querySelector('a');
 
+  // Аnimation
+  const animate = ({ duration, draw, timing }) => {
+    const start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      }
+
+      const progress = timing(timeFraction);
+
+      draw(progress);
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  };
+
   //Timer
   const countTimer = (deadline) => {
     const timerHours = document.querySelector('#timer-hours');
@@ -445,6 +465,62 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
+  // Калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block');
+    const calcSquare = document.querySelector('.calc-square');
+    const calcCount = document.querySelector('.calc-count');
+    const calcDay = document.querySelector('.calc-day');
+    const totalValue = document.getElementById('total');
+    let calcType = document.querySelector('.calc-type');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+      if (typeValue && squareValue) {
+        total = Math.round(price * typeValue * squareValue * dayValue * countValue);
+      }
+
+      // Анимация total
+      const totalAnim = () => {
+        animate({
+          duration: 700,
+          timing: (timeFraction) => {
+            return timeFraction;
+          },
+          draw: (progress) => {
+            totalValue.textContent = Math.round(total * progress);
+          },
+        });
+      };
+
+      totalAnim(total);
+      if (total === 0) {
+        calcSquare.value = '';
+        calcCount.value = '';
+        calcDay.value = '';
+      }
+    };
+
+    calcBlock.addEventListener('change', (evt) => {
+      if (evt.target.matches('select') || evt.target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+
   countTimer('27 july 2021');
   toggleMenu();
   togglePopUp();
@@ -453,4 +529,5 @@ window.addEventListener('DOMContentLoaded', () => {
   ourTeam();
   maskInput();
   validInput();
+  calc();
 });
