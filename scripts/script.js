ba6f7ea655667e067d1702a533c15ea75ef1d785
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-27 13:14:52 +0300

    Реализовал до конца валидацию телефона

--- scripts/script.js
+++ scripts/script.js
@@ -330,6 +330,45 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
+  // Корректировка телефона
+  const maskPhone = function (selector, masked = '+7 (___) ___-__-__') {
+    const elems = document.querySelectorAll(selector);
+
+    const mask = function (event) {
+      const keyCode = event.keyCode;
+      const template = masked,
+        def = template.replace(/\D/g, ''),
+        val = this.value.replace(/\D/g, '');
+      let i = 0,
+        newValue = template.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        });
+      i = newValue.indexOf('_');
+      if (i != -1) {
+        newValue = newValue.slice(0, i);
+      }
+      let reg = template
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+          return '\\d{1,' + a.length + '}';
+        })
+        .replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
+      if (!reg.test(this.value) || this.value.length < 5 || (keyCode > 47 && keyCode < 58)) {
+        this.value = newValue;
+      }
+      if (event.type === 'blur' && this.value.length < 5) {
+        this.value = '';
+      }
+    };
+
+    for (const elem of elems) {
+      elem.addEventListener('input', mask);
+      elem.addEventListener('focus', mask);
+      elem.addEventListener('blur', mask);
+    }
+  };
+
   // Валидация
   const maskInput = () => {
     document.body.addEventListener('input', (event) => {
@@ -358,7 +397,7 @@ window.addEventListener('DOMContentLoaded', () => {
       // Номер телефона
       if (target.name === 'user_phone') {
         target.setAttribute('type', 'text');
-        target.value = target.value.replace(/[^0-9\-()|(\+)]/g, '');
+        maskPhone('.form-phone');
       }
     });
   };
@@ -394,7 +433,7 @@ window.addEventListener('DOMContentLoaded', () => {
       };
 
       // Корректировка пробелов др. знаков в поле Ваше сообщение
-      if (target.name === 'user_message' || target.name === 'user_name') {
+      if (target.name === 'user_message' || target.name === 'user_name' || target.name === 'user_phone') {
         const changeReg = [/\s+/gm, /-+/gm, /,+/gm, /;+/gm, /:+/gm, /\.+/gm];
         const changeSymbol = [' ', '-', ',', ';', ':', '.'];
 
@@ -441,7 +480,7 @@ window.addEventListener('DOMContentLoaded', () => {
       if (target.name === 'user_email') {
         const correctMail = /^[\w\-\.\!\~\*\']+@[\w\-\.\!\~\*\']+(\.[a-z]{2,})$/;
         if (!correctMail.test(target.value)) {
-          target.value = '';
+          // target.value = '';
           correctBase.correctMail = false;
           showError(true);
         } else {
@@ -450,10 +489,10 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       }
       // валидация телефона
-      if (target.name === 'user_phone') {
-        target.value = target.value.replace(/^\+\d{1}\s/g, '+7 ');
+      if (event.target.name === 'user_phone') {
+        event.target.value = event.target.value.replace(/^\+\d{1}\s/g, '+7 ');
         // проверка на количество цифр
-        const corrNum = target.value.replace(/[\s\+\(\)-]*/g, '');
+        const corrNum = event.target.value.replace(/[\s\+\(\)-]*/g, '');
         if (corrNum.length < 11) {
           correctBase.correctTel = false;
           showError(true);
